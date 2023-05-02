20da9f45386ea793651791f99f0caa50a644e300
Diego Casas <diego.casas@wgu.edu>
2023-05-01 22:50:05 -0600

    Add splash

--- app/src/main/java/com/example/airbarchallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/example/airbarchallenge/presentation/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         viewModel.getTopRatedShowList()
         setContent {
             TopRatedListScreen(viewModel = viewModel)
--- app/src/main/java/com/example/airbarchallenge/presentation/screens/DetailsScreen.kt
+++ app/src/main/java/com/example/airbarchallenge/presentation/screens/DetailsScreen.kt
@@ -1,9 +1,11 @@
 package com.example.airbarchallenge.presentation.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -66,32 +69,38 @@ fun DetailsScreen(
 
 @Composable
 fun ShowDetails(showEntity: ShowEntity) {
-    Column(
+    Box(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
-            .testTag(DETAILS_TEST_TAG)
+            .fillMaxSize()
+            .background(color = Color.LightGray)
     ) {
-        AsyncImage(
-            model = showEntity.posterPath?.completeImagePath(),
-            placeholder = painterResource(R.drawable.baseline_downloading_24),
-            error = painterResource(R.drawable.baseline_error_24),
-            contentDescription = showEntity.name.orEmpty(),
+        Column(
             modifier = Modifier
+                .padding(16.dp)
                 .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(top = 60.dp),
-            contentScale = ContentScale.Crop
-        )
-        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
-            Text(
-                text = showEntity.name.orEmpty(),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(vertical = 16.dp)
+                .verticalScroll(rememberScrollState())
+                .testTag(DETAILS_TEST_TAG)
+        ) {
+            AsyncImage(
+                model = showEntity.posterPath?.completeImagePath(),
+                placeholder = painterResource(R.drawable.baseline_downloading_24),
+                error = painterResource(R.drawable.baseline_error_24),
+                contentDescription = showEntity.name.orEmpty(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(top = 60.dp),
+                contentScale = ContentScale.Crop
             )
-            Text(text = showEntity.overview.orEmpty(), lineHeight = 24.sp)
+            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                Text(
+                    text = showEntity.name.orEmpty(),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(vertical = 16.dp)
+                )
+                Text(text = showEntity.overview.orEmpty(), lineHeight = 24.sp)
+            }
         }
     }
 }
